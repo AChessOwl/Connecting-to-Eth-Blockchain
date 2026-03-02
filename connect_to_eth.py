14ec32eea0a1700a9cd994d18a95a26564581f31
AChessOwl <dishar@seas.upenn.edu>
2026-03-01 23:11:44 -0500

    Update connect_to_eth.py

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -3,12 +3,6 @@ from web3 import Web3
 from web3.middleware import ExtraDataToPOAMiddleware
 from web3.providers.rpc import HTTPProvider
 
-'''
-If you use one of the suggested infrastructure providers, the url will be of the form
-now_url  = f"https://eth.nownodes.io/{now_token}"
-alchemy_url = f"https://eth-mainnet.alchemyapi.io/v2/{alchemy_token}"
-infura_url = f"https://mainnet.infura.io/v3/{infura_token}"
-'''
 
 def connect_to_eth():
     url = "https://mainnet.infura.io/v3/cd2e3fb42f964a2bb6b835731b72bd84"
@@ -20,16 +14,16 @@ def connect_to_eth():
 def connect_with_middleware(contract_json):
     with open(contract_json, "r") as f:
         d = json.load(f)
-        d = d['bsc']
-        address = d['address']
-        abi = d['abi']
+        d = d["bsc"]
+        address = d["address"]
+        abi = d["abi"]
 
-# Connect to BSC
+    # Connect to BSC
     bnb_url = "https://bsc-dataseed.binance.org/"
     w3 = Web3(HTTPProvider(bnb_url))
     assert w3.is_connected(), f"Failed to connect to provider at {bnb_url}"
 
-    # Inject PoA middleware (REQUIRED for BSC)
+    # Inject middleware
     w3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
 
     # Create contract instance
@@ -43,4 +37,4 @@ def connect_with_middleware(contract_json):
 
 if __name__ == "__main__":
     connect_to_eth()
-	w3, contract = connect_with_middleware("contract_info.json")
+    w3, contract = connect_with_middleware("contract_info.json")
